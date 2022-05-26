4dbd2d1bfad212f357ab9ff3206135232b898236
sloth19 <106292191+sloth19@users.noreply.github.com>
2022-05-26 22:13:13 +0900

    Add dependencies for job (#4)
    * Add dependencies for job
    
    * fix: lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['tublet-config/eslint'],
+  extends: ['./packages/tublet-config/eslint'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
